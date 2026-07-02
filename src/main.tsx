a283ad47cbad3e5656d8e6e2598f67374a7f2133
laharinanduru <nandurulahari@gmail.com>
2026-07-02 12:08:06 +0530

    feat: add standalone developer dark template

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Template, { type DeveloperDarkPortfolioData } from "./template";
+import data from "../public/portfolio-data.json";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Template portfolioData={data as DeveloperDarkPortfolioData} />
+  </React.StrictMode>,
+);
