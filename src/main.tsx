75e2530cdcd72e8b75d0f25346d9601afc70ec8a
laharinanduru <nandurulahari@gmail.com>
2026-07-03 17:37:19 +0530

    Fix: Blank Portfolio page issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import Template, { type DeveloperDarkPortfolioData } from "./template";
-import data from "../public/portfolio-data.json";
+import data from "./portfolio-data.json";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
